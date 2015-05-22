e0a07861f993db1711383c2e733c7965c3e2d9fb
xd-deng <xd-deng@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-22 12:08:03 +0000

    test on the www file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-text-2-speech@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,8 +43,8 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
-
+<p> Hi, this is Xiaodong, from China, and working in National University of Singapore currently.  </p>
+<p> Happy to communicate with you.</p>
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
 </body>
