c2e86a3ecd070e07fae82aa97733328774122c1a
xd-deng <xd-deng@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-24 08:26:41 +0000

    add the functions for MicroSoft API
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-text-2-speech@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Rtts/R/tts_MS.R
@@ -0,0 +1,6 @@
+# http://www.projectoxford.ai/demo/speech#text2speech
+
+tts_MS <- function(text="hello world",
+                   destdestfile=paste(getwd(),"/RTTS.flv",sep="")){
+  return(NULL)
+}
